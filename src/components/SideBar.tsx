485b0edc2fd630c5a52f15e0e84a2d59f2cc17b5
Hygor Azevedo <hygor.azevedo@gmail.com>
2021-06-19 00:28:44 -0300

    desafio 02 ignite concluido

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { Button } from '../components/Button';
+import { useEffect, useState } from 'react';
+
+import { api } from '../services/api';
+
+interface SideBarProps {
+  selectedGenreId: number;
+  handleClickButton: Function;
+}
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
+  const [genres, setGenres] = useState<Genre[]>([]);
+
+  useEffect(() => {
+    api.get<Genre[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
